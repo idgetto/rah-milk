e926c1760452f51798265d7f4ed732409eea971d
Isaac Getto <isaac.getto@gmail.com>
2015-09-23 02:44:54 -0400

    create StatusCode

---
+++ include/response/status_code.h
@@ -0,0 +1,7 @@
+#ifndef STATUS_CODE_H
+#define STATUS_CODE_H
+
+class StatusCode {
+};
+
+#endif
---
+++ test/response/status_code_test.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+#include "response/status_code.h"
+
+TEST(StatusCode, constructor) {
+    StatusCode status;
+}
